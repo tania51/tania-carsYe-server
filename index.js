81d121aa40f161825dbe04b25165aaa1e0957b84
Tania Akter Bristy <taniaakterbristybd@gmail.com>
2023-10-21 19:00:00 +0600

    delete post get everything works perfectly

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.get('/', (req, res) => {
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jwathvu.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
