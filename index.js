d63f4e1e3316dd59473f17997f7c5db42bc0bc97
Tania Akter Bristy <taniaakterbristybd@gmail.com>
2023-12-13 18:45:11 +0600

    reposity changed

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ require('dotenv').config();
 const port = process.env.PORT || 5001;
 const app = express();
 
+// const corsOptions ={
+//   origin:'*', 
+//   credentials:true,    
+//   optionSuccessStatus:200,
+// }
+
+// app.use(cors(corsOptions))
+
 app.use(cors());
 app.use(express.json());
 
@@ -30,7 +38,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // get all cars for display
     const carCollection = client.db("carsYe").collection("allCars");
@@ -122,7 +130,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
